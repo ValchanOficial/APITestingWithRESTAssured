4b3ada7265b0fceb238e9d945c238f9f3887b5d1
Valéria Padilha de Vargas <valeria_padilha@yahoo.com.br>
2019-06-09 23:17:16 -0300

    Validações

--- restassured/src/main/java/br/com/testing/restassured/HelloWorldTest.java
+++ restassured/src/main/java/br/com/testing/restassured/HelloWorldTest.java
@@ -1,8 +1,29 @@
 package br.com.testing.restassured;
 
-import static io.restassured.RestAssured.*;
+import static io.restassured.RestAssured.get;
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.CoreMatchers.notNullValue;
+import static org.hamcrest.Matchers.allOf;
+import static org.hamcrest.Matchers.anyOf;
+import static org.hamcrest.Matchers.contains;
+import static org.hamcrest.Matchers.containsInAnyOrder;
+import static org.hamcrest.Matchers.containsString;
+import static org.hamcrest.Matchers.endsWith;
+import static org.hamcrest.Matchers.greaterThan;
+import static org.hamcrest.Matchers.hasItem;
+import static org.hamcrest.Matchers.hasItems;
+import static org.hamcrest.Matchers.hasSize;
+import static org.hamcrest.Matchers.is;
+import static org.hamcrest.Matchers.isA;
+import static org.hamcrest.Matchers.lessThan;
+import static org.hamcrest.Matchers.not;
+import static org.hamcrest.Matchers.startsWith;
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertThat;
 import static org.junit.Assert.assertTrue;
+
+import java.util.Arrays;
+import java.util.List;
 
 import org.junit.Test;
 
@@ -39,5 +60,42 @@ public class HelloWorldTest {
 		.then()
 		//.assertThat()
 		.statusCode(200);//assertions
+	}
+	
+	@Test
+	public void matchersComHamcrest() {
+		//http://hamcrest.org/JavaHamcrest/javadoc/1.3/org/hamcrest/Matchers.html
+		assertThat("Maria", is("Maria"));
+		assertThat(128, is(128));
+		
+		assertThat(128, isA(Integer.class));
+		assertThat(128d, isA(Double.class));
+		
+		assertThat(128d, greaterThan(120d));
+		assertThat(128d, lessThan(130d));
+		
+		List<Integer> impares = Arrays.asList(1,3,5,7,9);
+		assertThat(impares, hasSize(5));
+		assertThat(impares, contains(1,3,5,7,9)); //em ordem diferente dá problema
+		assertThat(impares, containsInAnyOrder(9,3,7,5,1)); //testa em qualquer ordem
+		assertThat(impares, hasItem(1));
+		assertThat(impares, hasItems(1,5));
+		
+		assertThat("Maria", is(not("João")));
+		assertThat("Maria", not("João"));
+		assertThat("Maria", anyOf(is("Maria"), is("João")));//OU
+		assertThat("Maria", allOf(startsWith("Mar"),endsWith("ia"),containsString("ari")));
+	}
+	
+	@Test
+	public void validandoBody() {
+		given()
+		.when()
+			.get("http://restapi.wcaquino.me/ola")//ação
+		.then()
+			.statusCode(200)
+			.body(is("Ola Mundo!"))
+			.body(containsString("Mundo"))
+			.body(notNullValue());
 	}
 }
---
+++ restassured/src/main/java/br/com/testing/restassured/UserJSONTest.java
@@ -0,0 +1,149 @@
+package br.com.testing.restassured;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.CoreMatchers.containsString;
+import static org.hamcrest.CoreMatchers.hasItem;
+import static org.hamcrest.Matchers.allOf;
+import static org.hamcrest.Matchers.closeTo;
+import static org.hamcrest.Matchers.contains;
+import static org.hamcrest.Matchers.greaterThan;
+import static org.hamcrest.Matchers.lessThan;
+import static org.hamcrest.Matchers.hasItems;
+import static org.hamcrest.Matchers.hasSize;
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+import org.junit.Test;
+
+import io.restassured.RestAssured;
+import io.restassured.http.Method;
+import io.restassured.path.json.JsonPath;
+import io.restassured.response.Response;
+
+public class UserJSONTest {
+	
+	@Test
+	public void deveFicarPrimeiroNivel() {
+		given()
+		.when()
+			.get("http://restapi.wcaquino.me/users/1")
+		.then()
+			.statusCode(200)
+			.body("id", is(1))
+			.body("name",containsString("Silva"))
+			.body("age", greaterThan(1));
+	}
+	
+	@Test
+	public void deveVerificarPrimeiroNivelComOutrasFormas() {
+		Response response = RestAssured.request(Method.GET, "http://restapi.wcaquino.me/users/1");
+		
+		assertEquals(new Integer(1), response.path("id"));
+		assertEquals(new Integer(1), response.path("%s","id"));
+		
+		//jsonpath
+		JsonPath jpath = new JsonPath(response.asString());
+		assertEquals(1, jpath.getInt("id"));
+		
+		//from
+		//int id = JsonPath.from(response.asString()).getInt("id");
+		//assertEquals(1, id);
+		assertEquals(1, JsonPath.from(response.asString()).getInt("id"));
+	}
+	
+	@Test
+	public void deveVerificarSegundoNivel() {
+		given()
+		.when()
+			.get("http://restapi.wcaquino.me/users/2")
+		.then()
+			.statusCode(200)
+			.body("name",containsString("Joaquina"))
+			.body("endereco.rua", is("Rua dos bobos"));
+	}
+	
+	@Test
+	public void deveVerificarLista() {
+		given()
+		.when()
+			.get("http://restapi.wcaquino.me/users/3")
+		.then()
+			.statusCode(200)
+			.body("name",containsString("Ana"))
+			.body("filhos", hasSize(2))
+			.body("filhos[0].name", is("Zezinho"))
+			.body("filhos[1].name", is("Luizinho"))
+			.body("filhos.name", hasItem("Zezinho"))
+			.body("filhos.name", hasItems("Zezinho","Luizinho"));
+	}
+	
+	@Test
+	public void deveVerificarErro() {
+		given()
+		.when()
+			.get("http://restapi.wcaquino.me/users/4")
+		.then()
+			.statusCode(404)
+			.body("error", is("Usuário inexistente"));
+	}
+	
+	@Test
+	public void deveVerificarListaRaiz() {
+		given()
+		.when()
+			.get("http://restapi.wcaquino.me/users")
+		.then()
+			.statusCode(200)
+			//.body("", hasSize(3))//raiz
+			.body("$", hasSize(3))//$ = raiz
+			.body("name", hasItems("João da Silva","Maria Joaquina","Ana Júlia"))
+			.body("age[1]", is(25))
+			.body("filhos.name", hasItem(Arrays.asList("Zezinho","Luizinho")))
+			.body("salary", contains(1234.5678f,2500,null));
+	}
+	
+	@Test
+	public void deveFazerVerificacoesAvancadas() {
+		given()
+		.when()
+			.get("http://restapi.wcaquino.me/users")
+		.then()
+			.statusCode(200)
+			.body("$", hasSize(3))
+			.body("age.findAll{it <= 25}.size()",is(2))//idade menor ou igual a 25
+			.body("age.findAll{it <= 25 && it > 20}.size()",is(1))
+			.body("findAll{it.age <= 25 && it.age > 20}.name",hasItem("Maria Joaquina")) //retorna lista
+			.body("findAll{it.age <= 25}[0].name",is("Maria Joaquina")) //trnasforma em object
+			.body("findAll{it.age <= 25}[-1].name",is("Ana Júlia"))
+			.body("find{it.age <= 25 && it.age > 20}.name",is("Maria Joaquina"))//pega 1 registro
+			.body("findAll{it.name.contains('n')}.name", hasItems("Maria Joaquina","Ana Júlia"))
+			.body("findAll{it.name.length() > 10}.name", hasItems("João da Silva","Maria Joaquina"))
+			.body("name.collect{it.toUpperCase()}", hasItem("MARIA JOAQUINA"))
+			.body("name.findAll{it.startsWith('Maria')}.collect{it.toUpperCase()}", hasItem("MARIA JOAQUINA"))
+			.body("name.findAll{it.startsWith('Maria')}.collect{it.toUpperCase()}", allOf(hasItem("MARIA JOAQUINA"), hasSize(1)))
+			.body("age.collect{it * 2}", hasItems(60,50,40))
+			.body("id.min()", is(1))
+			.body("salary.max()", is(2500))
+			.body("salary.min()", is(1234.5678f))
+			.body("salary.findAll{it != null}.sum()", is(closeTo(3734.5678,0.001)))
+			.body("salary.findAll{it != null}.sum()", allOf(greaterThan(3000d),lessThan(5000d)));
+	}
+	
+	@Test
+	public void devoUnirJsonPathComJava() {
+		ArrayList<String> names = 
+			given()
+			.when()
+				.get("http://restapi.wcaquino.me/users")
+			.then()
+				.statusCode(200)		
+				.extract().path("name.findAll{it.startsWith('Maria')}");
+		assertEquals(1, names.size());
+		assertTrue(names.get(0).equalsIgnoreCase("Maria Joaquina"));
+		assertEquals(names.get(0).toUpperCase(), "maria joaquina".toUpperCase());;
+	}
+}
